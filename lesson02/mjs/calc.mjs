f2fdc87df8d024a380b9f243114d6e1726ef2bb2
tempuraguruguru <takuno125m@gmail.com>
2024-08-06 15:56:05 +0900

    ファイルとフォルダの構造修正とECMAScriptを使うための.mjs追加

---
+++ lesson02/mjs/calc.mjs
@@ -0,0 +1,11 @@
+// ECMAScriptを使う場合は .js ではなく .mjs にする必要あり
+// import, export でモジュールの公開と読み込みが可能
+export const num = 1;
+
+export const add = (a, b) => {
+    return a + b;
+};
+
+export const sub = (a, b) => {
+    return a - b;
+};
