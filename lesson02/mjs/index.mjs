d1b359e0d07bb93027f647550ba863bb19ba8983
tempuraguruguru <takuno125m@gmail.com>
2024-08-06 16:14:09 +0900

    関数に対応

--- lesson02/mjs/index.mjs
+++ lesson02/mjs/index.mjs
@@ -1,4 +1,12 @@
-import { num, add, sub } from './calc.mjs';
+// defaultがついてるやつは読み込み方が変わる
+import defaultCalc, { num, add, sub, pow } from './calc.mjs';
+
+// 呼び出したタイミングで読み込まれる
+// import('./calc.mjs').then(
+//     (module) => {
+//         console.log(module.add(1, 2))
+//     }
+// )
 
 console.log(num);
 
@@ -7,3 +15,8 @@ console.log(res); // 4
 
 res = sub(3, 1);
 console.log(res); // 2
+
+const value = pow(2, 1);
+console.log(value);
+
+defaultCalc();
