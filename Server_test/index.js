9796aacd9dc0e5aa811d469c81c6acc407dc8b08
tempuraguruguru <takuno125m@gmail.com>
2024-08-08 17:03:04 +0900

    httpサーバからExpress(webフレームワーク)に再構築

--- Server_test/index.js
+++ Server_test/index.js
@@ -1,15 +1,14 @@
 const { isUtf8 } = require('buffer');
-const http = require('http');
-const { encode } = require('punycode');
+const express = require('express');
+const app = express();
 
-http.createServer((req, res) => {
-    if (req.url === '/' && req.method === 'GET') {
-        res.statusCode = 200;
-        res.write('hello world\n元気か？', isUtf8);
-        res.end();
-    } else {
-        res.statusCode = 404;
-        res.write('Not Found\n');
-        res.end();
-    }
-}).listen(3000);
+// GET '/' （トップ）アクセス時の挙動
+app.get('/', (req, res) => {
+    res.status(200).send('hello world\n');
+});
+
+// ポート: 3000でサーバーを起動
+app.listen(3000, () => {
+    // サーバー起動後に呼び出されるCallback
+    console.log('start listening');
+});
