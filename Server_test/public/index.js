4c09fa09601b997658c3d0ae6054614f4a6627f8
tempuraguruguru <takuno125m@gmail.com>
2024-08-08 18:35:41 +0900

    alphaなどのリストの要素をクリックするとアラートが表示

---
+++ Server_test/public/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelectorAll('.user-name').forEach((elem) => {
+        elem.addEventListener('click', (event) => {
+            alert(event.target.innerHTML);
+        });
+    });
+});
