0112e00bb8bfe2e8871e4051437802ffdaa07913
tempuraguruguru <takuno125m@gmail.com>
2024-08-08 18:41:50 +0900

    ボタンと入力欄の追加

--- Server_test/public/index.js
+++ Server_test/public/index.js
@@ -4,4 +4,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
             alert(event.target.innerHTML);
         });
     });
+    document.querySelector('.send-button').addEventListener('click', (event) => {
+        const newElement = document.createElement('li');
+        const text = document.querySelector('.input-text').value;
+        newElement.innerHTML = text;
+        document.querySelector('.user-list').appendChild(newElement);
+    });
 });
