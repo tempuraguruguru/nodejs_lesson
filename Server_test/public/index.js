de2023d2684b86c9110bf823399a0e5b8c057575
tempuraguruguru <takuno125m@gmail.com>
2024-08-11 01:54:59 +0900

    ボタンを押したらリロードされるように設定

--- Server_test/public/index.js
+++ Server_test/public/index.js
@@ -10,14 +10,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
         const text = document.querySelector('.input-text').value;
         newElement.innerHTML = text;
         document.querySelector('.user-list').appendChild(newElement);
-        fetch('/api/user-add',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
+        fetch('/api/user-add',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) });
+        window.location.reload();
     });
 
     document.querySelector('.send-button2').addEventListener('click', (event) => {
         const text = document.querySelector('.delete-text').value;
         let list_element = document.getElementById(text);
         list_element.remove();
-        fetch('/api/user-delete',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
+        fetch('/api/user-delete',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) });
+        window.location.reload();
     });
 
     document.querySelector('.send-button3').addEventListener('click', (event) => {
@@ -26,6 +28,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         const list_element = document.getElementById(pre);
         list_element.innerHTML = next;
         list_element.id = next;
-        fetch('/api/user-update',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: pre, new_name: next}) })
+        fetch('/api/user-update',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: pre, new_name: next}) });
+        window.location.reload();
     });
 });
