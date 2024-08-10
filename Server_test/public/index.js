0aa985ebd298578f363ead6399b07c80ed05c589
tempuraguruguru <takuno125m@gmail.com>
2024-08-11 01:49:49 +0900

    アーティスト名の追加、削除、変更をリアムタイムで更新

--- Server_test/public/index.js
+++ Server_test/public/index.js
@@ -4,23 +4,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
             alert(event.target.innerHTML);
         });
     });
+
     document.querySelector('.send-button1').addEventListener('click', (event) => {
         const newElement = document.createElement('li');
         const text = document.querySelector('.input-text').value;
         newElement.innerHTML = text;
         document.querySelector('.user-list').appendChild(newElement);
-    });
-    document.querySelector('.send-button1').addEventListener('click', (event) => {
-        const text = document.querySelector('.input-text').value;
         fetch('/api/user-add',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
     });
+
     document.querySelector('.send-button2').addEventListener('click', (event) => {
         const text = document.querySelector('.delete-text').value;
+        let list_element = document.getElementById(text);
+        list_element.remove();
         fetch('/api/user-delete',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: text }) })
     });
+
     document.querySelector('.send-button3').addEventListener('click', (event) => {
         const pre = document.querySelector('.previous-text').value;
         const next = document.querySelector('.next-text').value;
+        const list_element = document.getElementById(pre);
+        list_element.innerHTML = next;
+        list_element.id = next;
         fetch('/api/user-update',{ method: 'POST',headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ name: pre, new_name: next}) })
     });
 });
